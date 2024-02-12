f715b3c683b6e97f4693e038b8c558a56c2c7071
Derjoe123 <allotria2@gmail.com>
2024-02-12 09:08:52 +0100

    removed extra files

--- gitinit.sh
+++
@@ -1,4 +0,0 @@
-curl --request POST \
-	--url https://api.github.com/user/repos \
-	--header "Authorization: Bearer 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" \
-	--header "X-GitHub-Api-Version: 2022-11-28"
